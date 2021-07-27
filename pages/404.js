ec676b493c37fd02ab7281e6480fe6397192c68c
Jasmina <jasmina.eminovski@gmail.com>
2021-07-27 08:52:25 +0200

    Change page 404

--- pages/404.js
+++ pages/404.js
@@ -1,3 +1,8 @@
 export default function Custom404() {
-    return <h1>404 - Page Not Found</h1>
-  }
+    return (
+        <>
+            <h1>404 - Page Not Found</h1>
+            <p>This is a custom Not found page</p>
+        </>
+    )
+}
